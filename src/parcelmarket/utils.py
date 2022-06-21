84cf0df2e60ec9f387e18ff8a40ab0b23176e136
Andreas Alexopoulos <andalexo@gmail.com>
2022-06-21 14:21:55 +0300

    removes config setting from utils; adds logger

--- src/parcelmarket/utils.py
+++ src/parcelmarket/utils.py
@@ -8,67 +8,8 @@ import pandas as pd
 import numpy as np
 import shapefile as shp
 
-ENV_VARS = ["PARAMS_FILE", "SKIMTIME", "SKIMDISTANCE", "ZONES", "SEGS", "PARCELNODES",
-            "CEP_SHARES", "EXTERNAL_ZONES", "CONSOLIDATION_POTENTIAL",
-            "ZEZ_SCENARIO"]
-BOOL_VALUES = ('true', 't', 'on', '1', 'false', 'f', 'off', '0')
-BOOL_TRUE_VALUES = ('true', 't', 'on', '1')
-PARAMS_BOOL=["RUN_DEMAND_MODULE", "CROWDSHIPPING_NETWORK", "COMBINE_DELIVERY_PICKUP_TOUR",
-             "HYPERCONNECTED_NETWORK", "printKPI"]
-PARAMS_NUM=["PARCELS_PER_EMPL", "Local2Local", "CS_cust_willingness",
-            "PARCELS_MAXLOAD", "PARCELS_DROPTIME", "PARCELS_SUCCESS_B2C",
-            "PARCELS_SUCCESS_B2B", "PARCELS_GROWTHFREIGHT", "PARCELS_PER_HH_B2C",
-            "PARCELS_M", "PARCELS_DAYS", "PARCELS_M_HHS"]
 
-
-logger = getLogger("parcelgen.utils")
-
-
-def parse_env_values(env):
-    """Parses environment values.
-
-    :param env: The environment dictionary
-    :type env: dict
-    :raises KeyError: If a required key is missing
-    :raises ValueError: If the value of the key is invalid
-    :return: The configuration dictionary
-    :rtype: dict
-    """
-    config_env = {}
-    try:
-        config_env["LABEL"] = env['LABEL']
-        for key in PARAMS_BOOL:
-            config_env[key] = to_bool(env[key])
-        for key in PARAMS_NUM:
-            config_env[key] = float(env[key])
-    except KeyError as exc:
-        raise KeyError("Failed while parsing environment configuration") from exc
-    except ValueError as exc:
-        raise ValueError("Failed while parsing environment configuration") from exc
-
-    config_env["PARCELS_PER_HH_C2C"] = config_env["PARCELS_M"] / config_env["PARCELS_DAYS"] \
-        / config_env["PARCELS_M_HHS"]
-    config_env["PARCELS_PER_HH"] = config_env["PARCELS_PER_HH_C2C"] + \
-        config_env['PARCELS_PER_HH_B2C']
-
-    return config_env
-
-
-def to_bool(value):
-    """Translates a string to boolean value.
-
-    :param value: The input string
-    :type value: str
-    :raises ValueError: raises value error if the string is not recognized.
-    :return: The translated boolean value
-    :rtype: bool
-    """
-    val = str(value).lower()
-    if val not in BOOL_VALUES:
-        raise ValueError(f'error: {value} is not a recognized boolean value {BOOL_VALUES}')
-    if val in BOOL_TRUE_VALUES:
-        return True
-    return False
+logger = getLogger("parcelmarket.utils")
 
 
 def get_traveltime(orig, dest, skim, n_zones, time_fac):
